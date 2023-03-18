35ced413329c6ecfbf7e2084296e0fb58258f0d9
Shobana <mathiarulshobana@gmail.com>
2023-03-18 20:12:30 +0100

    configured mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db.config');
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
