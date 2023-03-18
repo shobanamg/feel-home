28f337d649627104b284120a7687b147325e46b2
Shobana <mathiarulshobana@gmail.com>
2023-03-18 17:36:38 +0100

    Initial express server setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`\`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+});
